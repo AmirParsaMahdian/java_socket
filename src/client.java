60b50d649421598e50e95d4f6ae6882f5b8df9de
amir parsa Mahdian <amirparsamahdian@gmail.com>
2018-02-03 20:34:32 +0330

    Implemented threads so both receiving and sending are possible at the same time.

--- src/client.java
+++ src/client.java
@@ -4,23 +4,63 @@ import java.util.Scanner;
 
 public class client {
 
-    public static void main(String[] args) {
-        try{
-            Socket s = new Socket("192.168.1.2",7070);
-            String data = " ";
-            
-            while (!data.equals("exit") ){
-                DataInputStream clientin = new DataInputStream(s.getInputStream());
-                data = clientin.readUTF();
-                System.out.println("Received: " + data);
+    private static String host = "192.168.1.6";
+    private static int port = 5050;
 
-                DataOutputStream clientout = new DataOutputStream(s.getOutputStream());
-                Scanner usrin = new Scanner(System.in);
-                clientout.writeUTF(usrin.nextLine());
-            }
+    public static void main(String[] args) {
+
+        try{
+
+            Socket s = new Socket(host, port);
+            final Scanner scanner = new Scanner(System.in);
+
+            DataInputStream receiving = new DataInputStream(s.getInputStream());
+            //BufferedReader receiving = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream sending = new DataOutputStream(s.getOutputStream());
+            //PrintWriter sending = new PrintWriter(s.getOutputStream());
+
+
+            Thread send = new Thread(new Runnable() {
+
+                String msgout;
+
+                @Override
+                public void run() {
+
+                    while (true){
+
+                        msgout = scanner.nextLine();
+
+                        try {
+                            sending.writeUTF(msgout);
+                            sending.flush();
+
+                        } catch (Exception e) {}
+                    }
+                }
+            });
+            send.start();
+
+            Thread receive = new Thread((new Runnable() {
+
+                String msgin;
+
+                @Override
+                public void run() {
+
+                    while (true){
+
+                        try {
+                            msgin = receiving.readUTF();
+
+                        } catch (Exception e) {}
+
+                        System.out.println("Received: " + msgin);
+                    }
+                }
+            }));
+            receive.start();
 
         }catch(Exception e){}
-
-
     }
 }
--- src/server.java
+++ src/server.java
@@ -2,28 +2,67 @@ import java.net.*;
 import java.io.*;
 import java.util.Scanner;
 
-public class server {
+public class server extends Thread{
 
     public static void main(String[] args) {
 
+        Socket s;
+
+        final Scanner scanner = new Scanner(System.in);
+
         try{
-            ServerSocket server = new ServerSocket(7070);
-            Socket s = server.accept();
-            System.out.println("Connected");
-            String data = " ";
 
-            while (data != "exit") {
-                DataOutputStream serverout = new DataOutputStream(s.getOutputStream());
-                Scanner usrin = new Scanner(System.in);
-                serverout.writeUTF(usrin.nextLine());
+            ServerSocket serverSocket = new ServerSocket(5050);
+            System.out.println("Server created.");
+            s = serverSocket.accept();
+            System.out.println("Client connected.");
 
-                DataInputStream serverin = new DataInputStream(s.getInputStream());
-                data = serverin.readUTF();
-                System.out.println("received: " + data);
+            DataInputStream receiving = new DataInputStream(s.getInputStream());
+            //BufferedReader receiving = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream sending = new DataOutputStream(s.getOutputStream());
+            //PrintWriter sending = new PrintWriter(s.getOutputStream());
 
-            }
+
+            Thread send = new Thread(new Runnable() {
+
+                String msgout;
+
+                @Override
+                public void run() {
+
+                    while (true){
+
+                        msgout = scanner.nextLine();
+
+                        try {
+                            sending.writeUTF(msgout);
+                            sending.flush();
+                        } catch (Exception e) {}
+                    }
+                }
+            });
+            send.start();
+
+            Thread receive = new Thread((new Runnable() {
+
+                String msgin;
+
+                @Override
+                public void run() {
+
+                    while (true){
+
+                        try {
+                            msgin = receiving.readUTF();
+
+                        } catch (Exception e) {}
+
+                        System.out.println("Received: " + msgin);
+                    }
+                }
+            }));
+            receive.start();
 
         }catch(Exception e){}
-
     }
 }
